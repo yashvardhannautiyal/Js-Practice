2c50a2c305ac0a7e06a71b7b605eb9b4cab01234
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2025-03-19 18:01:51 +0530

    files added

---
+++ 01_Basic_Syntax_Data_types.js
@@ -0,0 +1,46 @@
+// 1. Write a JavaScript program to print "Hello, World!" to the console.
+// console.log("hello world")
+
+
+// 2. Declare a variable and assign it a string value.
+// let a = "this is string";
+// console.log(typeof(a));
+
+
+// 3. Write a program to swap two variables without using a third variable.
+/* let num1 = 1
+let num2 =2
+
+//using third variable
+// let num3 = num1
+// num1 = num2
+// num2 = num3
+// console.log("num1 : " + num1);
+// console.log("num2 : " + num2);
+// console.log("num3 : " + num3);
+
+//without using third variable
+console.log("before swapping \n num1 : "+num1 + " num2 : " + num2);
+
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+console.log("after swapping \n num1 : "+num1 + " num2 : " + num2)
+*/
+
+
+/*
+// 4. Convert a string to a number and vice versa
+let str = "7685"
+let num = 87;
+
+//string to number
+let str2 = parseInt(str);
+console.log(typeof(str2));
+
+let num2 = num.toString()
+console.log(typeof(num2))
+
+*/
+
+
