1ab589b940e7ffe6f5beda251562210ed0742591
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2025-03-22 08:40:17 +0530

    loops and conditions practice

---
+++ 02_loops_conditionals.js
@@ -0,0 +1,62 @@
+/*
+// 1. Print numbers from 1 to 10 using a loop.
+for (let i = 1; i <= 10; i++) {
+    console.log(i);    
+}
+    */
+
+
+/*
+// 2. Print the multiplication table of a number.
+let num = 8;
+for(let  i = 1; i<=10; i++){
+    console.log(num + "*" + i + " = " + (num*i))
+}
+    */
+
+/*
+// 3. Find the sum of natural numbers up to a given number.
+let num = 98;
+let sum = 0;
+for(let i = 0; i<=num; i++){
+   sum += i;
+}
+console.log(sum);
+
+*/
+
+// 4. Write a program to reverse a number.
+// let num = 9098;
+
+
+
+/*
+// 5. Find the factorial of a number.
+// const does not updates the value outside the function block
+// let does it
+let num = 4;
+let fact = 1;
+for(let i = 1; i<=num; i++){
+    fact *= i
+     }
+     console.log(fact); 
+
+     */
+
+
+     /*
+    //  6. Check if a number is prime.
+    let n= 191;
+    function prime(num){
+    for(let i = 2; i<=num; i++){
+        if(num < i || num%2 == 0 || num%3 == 0){
+            return "number is not prime";
+        }
+        else{
+            return "number is prime";
+        }
+    }
+    }
+    console.log(prime(n));
+
+    */
